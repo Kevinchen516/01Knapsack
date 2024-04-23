36d3bb660f82fef03284531e8f27f58228408be1
Astronaut <90697232+Astronaut2050@users.noreply.github.com>
2024-04-23 17:25:43 -0400

    Delete Project/run_0.bat

--- Project/run_0.bat
+++
@@ -1,4 +0,0 @@
-python starter.py -inst ./DATA/DATASET/large_scale/large_1 -alg LS1 -time 300 -seed 10
-python starter.py -inst ./DATA/DATASET/large_scale/large_2 -alg LS1 -time 300 -seed 10
-python starter.py -inst ./DATA/DATASET/large_scale/large_3 -alg LS1 -time 300 -seed 10
-python starter.py -inst ./DATA/DATASET/large_scale/large_4 -alg LS1 -time 300 -seed 10
